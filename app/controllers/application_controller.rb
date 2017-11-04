bcd850c1216eedd72f5b4a3677b26c9de58792d7
Deborah Seow <deborahseow9@gmail.com>
2017-11-04 22:29:38 +0000

    created book & user controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < Sinatra::Base
 
   configure do
-   enable :sessions
-   set :session_secret, "secret"
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "password_security"
   end
-
 end
