98e95fa01da0078577f4a110fb13688079734eb9
Deborah Seow <deborahseow9@gmail.com>
2017-11-05 21:23:00 +0000

    Finished login and sign up forms and routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,20 +11,20 @@ use Rack::Flash
 
   post '/signup' do
     if params[:user][:username] == "" || params[:user][:password] == "" || params[:user][:email] == ""
-      redirect "/signup" #CAN WE DO FLASH AND NOT RELOAD SIGNUP PAGE? or RELOAD WITH SAME INFO?
+      redirect "/signup"
     else
       @user = User.new(username:  params[:user][:username], email: params[:user][:email], password: params[:user][:password])
-      @user.save
+      @user.save #user.save only authenticates a password is not an empty string, not the username or email
       session[:id] = @user.id
       redirect to "/users/#{@user.id}"
     end
   end
 
 #THINGS TO BE DONE:
-#In signup - make sure that people with the same email cannot sign up.
+#In signup - make sure that people with the same email cannot sign up. (We're not doing this one for Sinatra project)
+#Have some kind of message for wrong logins and sign
 
   get '/users/:id' do
-
     @user = User.find_by(id: params[:id])
     if logged_in? && current_user == @user
       erb :profile_page
@@ -46,7 +46,6 @@ use Rack::Flash
         session[:id] = @user.id
         redirect "/users/#{@user.id}"
     else
-      #use Flash Here!
       redirect "/login"
     end
   end
