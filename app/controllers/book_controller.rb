e6f51fa4e9618d3ed2686eaaa32886362f38a8a2
Deborah Seow <deborahseow9@gmail.com>
2017-11-08 22:12:15 +0000

    Moved author.erb to views folder, finished book edit route controller logic, added href links to detail pages

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -68,7 +68,7 @@ class BookController < ApplicationController
 
     @author = Author.find_by(id: params[:id])
 
-    erb :'/book/author'
+    erb :author
   end
 
   #SEE GENRE PAGE ------------
@@ -99,19 +99,21 @@ class BookController < ApplicationController
     erb :'book/edit'
   end
 
-  patch '/manga/edit/:id' do
+  post '/manga/edit/:id' do
     @book = Book.find_by(id: params[:id])
 
     author = params[:manga][:author].strip.split.map(&:capitalize).join(' ')
     genre = params[:manga][:genre].strip.split.map(&:capitalize).join(' ')
-
+    #Updates title if title box is filled out
     if params[:manga][:title] != ""
-      @book = Book.new(title: params[:manga][:title].split.map(&:capitalize).join(' '), volume: params[:manga][:volume])
+      @book.title = params[:manga][:title].split.map(&:capitalize).join(' ')
+    end
+    #Updates volume if volume box is filled out
+    if params[:manga][:volume] != ""
+    @book.volume = params[:manga][:volume]
     end
     #If both are authors filled, redirects
-    #If title is not filled out, redirects
-    #If both genres filled out, redirects
-    if author != "" && params[:manga][:author_id] != nil || genre != "" && params[:manga][:genre_ids] != nil || params[:manga][:title] == ""
+    if author != "" && params[:manga][:author_id] != nil
       redirect '/manga/new'
     #If top is filled out, but author exists, it assigns existing author
     elsif author != "" && Author.check(author)
@@ -123,26 +125,23 @@ class BookController < ApplicationController
     else
       @book.author = Author.find_by(id: params[:manga][:author_id])
     end
+    #First, we clear the genres array
+    @book.genres.delete_all
     #If top is filled out, but genre exists, it assigns existing genre
     if genre != "" && Genre.check(genre)
       @book.genres << Genre.find_by(name: genre)
     #If top is filled out and genre doesn't exist, it assigns top
-    elsif genre != ""
+    else genre != ""
       @book.genres << Genre.create(name: genre)
-    #If top isn't filled out, it assigns bottom
-    else
-      if params[:manga][:genre_ids] != nil
-        params[:manga][:genre_ids].each do |id|
-          @book.genres << Genre.find_by(id: id)
-        end
+    #No matter what, it assigns bottom
+    end
+    if params[:manga][:genre_ids] != nil
+      params[:manga][:genre_ids].each do |id|
+        @book.genres << Genre.find_by(id: id)
       end
     end
 
   @book.save
-    if logged_in?
-      current_user.books << @book
-    end
-    current_user
     erb :'/book/book'
   end
 end
