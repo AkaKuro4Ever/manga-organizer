723f9aaa7e0079b4686f887290e9081bc800497f
Deborah Seow <deborahseow9@gmail.com>
2017-11-29 21:27:34 +0000

    All things done with Daniel - just added validation and refactoring

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ Use this program to keep track of your manga and novels in your library, includi
 Usage
 
 To use this app, just clone, run rake db:migrate and then run shotgun. Everything should be set up.
+
+
+
+# New App
+
+Domain
+
+# Models
+
+User
+----
+email -> must be unique
+password -> must be secure
+has many manga
+
+Manga
+-----
+title:String -> must not be blank
+description:Text -> must not be blank
+author:String -> must not be blank
+belongs to a user
+
+# Requirements
+
+- A user must be logged in to create, update or destroy a manga
+- Only the user who created the manga can update, destroy or view the edit form
--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,7 +1,7 @@
+require 'pry'
 class BookController < ApplicationController
 
   get '/manga/new' do
-
     erb :'/book/new'
   end
 
@@ -16,56 +16,56 @@ class BookController < ApplicationController
     author = params[:manga][:author].strip.split.map(&:capitalize).join(' ')
     genre = params[:manga][:genre].strip.split.map(&:capitalize).join(' ')
 
-    if params[:manga][:title] != ""
+    if !params[:manga][:title].empty?
       @book = Book.new(title: params[:manga][:title].split.map(&:capitalize).join(' '), volume: params[:manga][:volume])
     end
     #If both are authors filled, redirects
     #If title is not filled out, redirects
-    if author != "" && params[:manga][:author_id] != nil || params[:manga][:title] == ""
+    if !author.empty? && params[:manga][:author_id] || params[:manga][:title] == ""
       redirect '/manga/error/1'
     #If top is filled out, but author exists, it assigns existing author
-    elsif author != "" && Author.check(author)
+  elsif !author.empty? && Author.check(author)
       @book.author = Author.find_by(name: author)
     #If top is filled out and author doesn't exist, it assigns top
-    elsif author != ""
+  elsif !author.empty?
       @book.author = Author.create(name: author)
     #If top isn't filled out, it assigns bottom
     else
       @book.author = Author.find_by(id: params[:manga][:author_id])
     end
     #If top is filled out, but genre exists, it assigns existing genre
-    if genre != "" && Genre.check(genre)
+    if !genre.empty? && Genre.check(genre)
       @book.genres << Genre.find_by(name: genre)
     #If top is filled out and genre doesn't exist, it assigns top
-    elsif genre != ""
+  elsif !genre.empty?
       @book.genres << Genre.create(name: genre)
     #If top isn't filled out, it assigns bottom
     end
-    if params[:manga][:genre_ids] != nil
+    if params[:manga][:genre_ids]
       params[:manga][:genre_ids].each do |id|
         @book.genres << Genre.find_by(id: id)
       end
     end
 
-  @book.save
-    if logged_in?
-      current_user.books << @book
+    if @book.save
+      if logged_in?
+        current_user.books << @book
+      end
+      current_user
+      erb :'/book/book'
+    else
+      @message = "It seems that you've had an error while creating a new manga! Make sure you only listed one author and have filled in a title."
+      erb :'book/new'
     end
-    current_user
-    erb :'/book/book'
   end
 
-  #ERROR WHEN FILLING OUT NEW MANGA FORM
-  get '/manga/error/1' do
-
-    erb :error
-  end
   #SEE INDIV MANGA BOOK PAGE ----------
   get '/manga/:id' do
     @book = Book.find_by(id: params[:id])
     if @book
       erb :'/book/book'
     else
+      @message = "This book does not exist. Please try to search for a different book."
       redirect to '/manga'
     end
   end
@@ -74,77 +74,80 @@ class BookController < ApplicationController
   post '/manga/:id/add' do
     @book = Book.find_by(id: params[:id])
     current_user.books << @book
+    if !current_user.books << @book
+      @message = "It seems there was an error editing this book from your collection. Please try again."
+    end
     redirect to "users/#{current_user.id}"
   end
 
   #REMOVE INDIV MANGA TO USER'S PROFILE PAGE---
   delete '/manga/:id/remove' do
     @book = Book.find_by(id: params[:id])
-    current_user.books.delete(@book)
-    redirect to "users/#{current_user.id}"
+    if !current_user.books.delete(@book)
+      @message = "It seems there was an error editing this book from your collection. Please try again."
+    end
+    redirect "users/#{current_user.id}"
+  end
+  #ERROR WHEN FILLING OUT DELETE MANGA --
+  get '/manga/error/1' do
+    erb :error
   end
 
   #EDIT INDIV MANGA BOOK DETAILS
   get '/manga/:id/edit' do
     @book = Book.find_by(id: params[:id])
-    erb :'book/edit'
+
+    if @book
+      erb :'/book/edit'
+    else
+      @message = "This book does not exist. Please try to search for a different book."
+      redirect to '/manga'
+    end
   end
 
-  put '/manga/:id' do
+  patch '/manga/:id' do
     @book = Book.find_by(id: params[:id])
 
     author = params[:manga][:author].strip.split.map(&:capitalize).join(' ')
     genre = params[:manga][:genre].strip.split.map(&:capitalize).join(' ')
-    #Updates title if title box is filled out
-    if params[:manga][:title] != ""
-      @book.title = params[:manga][:title].split.map(&:capitalize).join(' ')
+
+    @book.title = params[:manga][:title].split.map(&:capitalize).join(' ') if !params[:manga][:title].empty?
+    @book.volume = params[:manga][:volume] if !params[:manga][:volume].empty?
+
+    if !author.empty? && params[:manga][:author_id]
+      @message = "Please make sure you only list one author in either the text box or the list."
+      @book.author = Author.find_by(name: author) if !author.empty? && Author.check(author)
+      @book.author = Author.create(name: author) if !author.empty? && !Author.check(author)
+
+      @book.author = Author.find_by(id: params[:manga][:author_id]) if params[:manga][:author_id] && author.empty?
+      redirect to "/manga/#{@book.id}/edit"
     end
-    #Updates volume if volume box is filled out
-    if params[:manga][:volume] != ""
-    @book.volume = params[:manga][:volume]
-    end
-    #If both are authors filled, redirects
-    if author != "" && params[:manga][:author_id] != nil
-      redirect '/manga/new'
-    #If top is filled out, but author exists, it assigns existing author
-    elsif author != "" && Author.check(author)
-      @book.author = Author.find_by(name: author)
-    #If top is filled out and author doesn't exist, it assigns top
-    elsif author != ""
-      @book.author = Author.create(name: author)
-    else
-      nil
-    #If top isn't filled out, it assigns bottom
-    end
-    if params[:manga][:author_id] != nil
-      @book.author = Author.find_by(id: params[:manga][:author_id])
-    end
-    #First, we clear the genres array
+
     @book.genres.delete_all
-    #If top is filled out, but genre exists, it assigns existing genre
-    if genre != "" && Genre.check(genre)
-      @book.genres << Genre.find_by(name: genre)
-    #If top is filled out and genre doesn't exist, it assigns top
-    elsif genre != ""
-      @book.genres << Genre.create(name: genre)
-    #No matter what, it assigns bottom
-    else
-    end
-    if params[:manga][:genre_ids] != nil
-      params[:manga][:genre_ids].each do |id|
-        @book.genres << Genre.find_by(id: id)
-      end
+    @book.genres << Genre.find_by(name: genre) if !genre.empty? && Genre.check(genre)
+    @book.genres << Genre.create(name: genre) if !genre.empty?
+
+    if params[:manga][:genre_ids]
+      params[:manga][:genre_ids].each { |id| @book.genres << Genre.find_by(id: id) }
     else
       @book.genres = []
     end
 
-  @book.save
-    erb :'/book/book'
+    if @book.save
+
+      redirect to "/manga/#{@book.id}"
+    else
+      @message = "It seems that you've had an error while editing this manga! Make sure you only listed one author and have filled in a title."
+      erb :'/book/edit'
+    end
   end
 
   delete '/manga/:id' do
     @book = Book.find_by(id: params[:id])
-    @book.delete
-    redirect '/manga'
+    if @book.delete
+      redirect '/manga'
+    else
+      erb :error
+    end
   end
 end
